ea8fb3d1140f69e232acc19d847d66a151c4600f
Yazilimca <info@bizimbilisim.com>
2016-11-22 21:14:00 +0200

    Create db.php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+
+$db = new MysqliDb (Array (
+
+                'host' => 'localhost',
+
+                'username' => '*****', 
+
+                'password' => '*****',
+
+                'db'=> '****',
+
+                'port' => 3306,
+
+                'prefix' => '',
+
+                'charset' => 'utf8'));
